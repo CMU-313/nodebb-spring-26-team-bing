4d4ff04a0d7363490116e128047e91010f5b12a7
sliang-code <sihanlia@andrew.cmu.edu>
2026-02-10 19:41:02 +0000

    solving referencing/overwrite issue

--- src/topics/posts.js
+++ src/topics/posts.js
@@ -135,7 +135,7 @@ module.exports = function (Topics) {
 
 		postData.forEach((postObj, i) => {
 			if (postObj) {
-				postObj.user = postObj.uid ? userData[postObj.uid] : { ...userData[postObj.uid] };
+				postObj.user = { ...userData[postObj.uid] };
 				postObj.editor = postObj.editor ? editors[postObj.editor] : null;
 				postObj.bookmarked = bookmarks[i];
 				postObj.upvoted = voteData.upvotes[i];
