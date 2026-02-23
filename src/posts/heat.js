b51439292cfe919042f9eafac6883155ed4ff450
Elvin Seudieu <eseudieu@andrew.cmu.edu>
2026-02-23 14:38:50 +0000

    created new files for each sorting logic

---
+++ src/posts/heat.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const _ = require('lodash');
+
+const db = require('../database');
+const privileges = require('../privileges');
+
+module.exports = function (Posts) {
+    Posts.getHeat = async function (uid, start, stop, term) {
+
+    };
+};
---
+++ src/posts/most-comments.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const _ = require('lodash');
+
+const db = require('../database');
+const privileges = require('../privileges');
+
+module.exports = function (Posts) {
+    Posts.getMostComments = async function (uid, start, stop, term) {
+
+    };
+};
