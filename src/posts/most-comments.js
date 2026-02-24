822040973f4c1916e3b2be5c6a91bc284b3f70ac
Elvin Seudieu <eseudieu@andrew.cmu.edu>
2026-02-24 20:34:23 +0000

    reverted to state before sorting changes

---
+++ src/posts/most-comments.js
@@ -0,0 +1,13 @@
+/* eslint-disable @stylistic/js/indent */
+'use strict';
+
+const _ = require('lodash');
+
+const db = require('../database');
+const privileges = require('../privileges');
+
+module.exports = function (Posts) {
+    Posts.getMostComments = async function (uid, start, stop, term) {
+
+    };
+};
---
+++ src/posts/most_votes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const _ = require('lodash');
+
+const db = require('../database');
+const privileges = require('../privileges');
+
+module.exports = function (Posts) {
+    Posts.getMostVotes = async function (uid, start, stop, term) {
+
+    };
+};
