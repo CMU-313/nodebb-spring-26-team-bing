9fef86fb1af1d3ae11ded4c59cb5c0a8f4539245
pebble-fish <pebble.fishie@gmail.com>
2026-02-27 00:35:34 -0500

    Add post visibility handling to create, edit, and topic creation

---
+++ src/posts/visibility.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const user = require('../user');
+
+const VALID_MODES = new Set(['public', 'anonymous', 'instructors']);
+
+function normalizeVisibilityMode(mode, anonymous) {
+	if (VALID_MODES.has(mode)) {
+		return mode;
+	}
+
+	return anonymous === true || anonymous === 'true' ? 'anonymous' : 'public';
+}
+
+async function isViewerAdmin(uid) {
+	if (!parseInt(uid, 10)) {
+		return false;
+	}
+	return await user.isAdministrator(uid);
+}
+
+function canViewPost(post, uid, isAdmin) {
+	if (!post) {
+		return false;
+	}
+
+	const mode = normalizeVisibilityMode(post.visibilityMode, post.anonymous);
+	if (mode !== 'instructors') {
+		return true;
+	}
+
+	if (String(uid) === String(post.uid)) {
+		return true;
+	}
+
+	return !!isAdmin;
+}
+
+module.exports = {
+	VALID_MODES,
+	normalizeVisibilityMode,
+	isViewerAdmin,
+	canViewPost,
+};
